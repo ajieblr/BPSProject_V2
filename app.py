f5e274a8289523a2ca77b37a0e528b315675aa59
ajibulriski <ajibul.riski@gmail.com>
2024-07-10 13:40:29 +0700

    ganti ke flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/predict')
+def page1():
+    return render_template('predict.html')
+
+@app.route('/profile')
+def page2():
+    return render_template('profile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
